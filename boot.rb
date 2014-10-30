93e7681d6ac9155751983a4f47ae1cda442b960c
John Goldsmith <johngoldsm1@gmail.com>
2014-10-29 23:28:00 -0700

    Heroku db fix

--- boot.rb
+++ boot.rb
@@ -22,13 +22,13 @@ require "./config/environments"
 
 module Backlogg
   APP_ROOT = File.dirname(__FILE__)
-  CONFIG = YAML.load_file(File.join(APP_ROOT, 'config', 'config.yml'))
-  # RACK_ENV = ENV['RACK_ENV'] || 'development'
-  # DB_CONFIG = YAML.load_file(File.join(APP_ROOT, 'config', 'database.yml'))
+  CONFIG = YAML.load_file(File.join(APP_ROOT, "config", "config.yml"))
+  # RACK_ENV = ENV["RACK_ENV"] || "development"
+  # DB_CONFIG = YAML.load_file(File.join(APP_ROOT, "config", "database.yml"))
 end
 
-# Dir[Backlogg::APP_ROOT + '/config/initializers/*.rb'].each { |initializer| require initializer }
-Dir[Backlogg::APP_ROOT + '/lib/concerns/*.rb'].each { |concern| require concern }
-Dir[Backlogg::APP_ROOT + '/lib/models/*.rb'].each { |model| require model }
-Dir[Backlogg::APP_ROOT + '/lib/serializers/*.rb'].each { |serializer| require serializer }
-Dir[Backlogg::APP_ROOT + '/lib/controllers/*.rb'].each { |controller| require controller }
+# Dir[Backlogg::APP_ROOT + "/config/initializers/*.rb"].each { |initializer| require initializer }
+Dir[Backlogg::APP_ROOT + "/lib/concerns/*.rb"].each { |concern| require concern }
+Dir[Backlogg::APP_ROOT + "/lib/models/*.rb"].each { |model| require model }
+Dir[Backlogg::APP_ROOT + "/lib/serializers/*.rb"].each { |serializer| require serializer }
+Dir[Backlogg::APP_ROOT + "/lib/controllers/*.rb"].each { |controller| require controller }
--- config/environments.rb
+++ config/environments.rb
@@ -1,15 +1,15 @@
 Sinatra::Base.configure :production, :development do
   # if ENV["DATABASE_URL"]
-    # db = URI.parse(ENV["DATABASE_URL"] || "postgres://localhost/backlogg_development")
+    db = URI.parse(ENV["DATABASE_URL"] || "postgres://localhost/backlogg_development")
 
-    # ActiveRecord::Base.establish_connection(
-    #   :adapter  => db.scheme == "postgres" ? "postgresql" : db.scheme,
-    #   :host     => db.host,
-    #   :username => db.user,
-    #   :password => db.password,
-    #   :database => db.path[1..-1],
-    #   :encoding => "utf8"
-    # )
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == "postgres" ? "postgresql" : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => "utf8"
+    )
   # end
-  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || "postgres://localhost/backlogg_development")
+  # ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || "postgres://localhost/backlogg_development")
 end
