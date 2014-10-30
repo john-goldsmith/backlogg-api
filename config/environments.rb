dd77d3073e1142d7c890c56d86274129cfe06a20
John Goldsmith <johngoldsm1@gmail.com>
2014-10-29 22:47:04 -0700

    Heroku env var fix

--- config/environments.rb
+++ config/environments.rb
@@ -1,12 +1,14 @@
 Sinatra::Base.configure :production, :development do
-  db = URI.parse(ENV['DATABASE_URL'])
+  if ENV['DATABASE_URL']
+    db = URI.parse(ENV['DATABASE_URL'])
 
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
+  end
 end
